146763c0c6d4c003845387c11dfb6deef4b3968f
dc-fgulmez <fatih@datacose.com>
2024-04-02 16:17:23 +0300

    mappers

--- platforms/shipstation/initializers.py
+++ platforms/shipstation/initializers.py
@@ -1,4 +1,8 @@
 from datacose.shipstation.client import ShipStation
 from datacose.shipstation.initializer import ShipStationModelInitializer
 
-SHIPSTATION_ADMIN = ShipStationModelInitializer(sdk=ShipStation("***", "***"))
+SHIPSTATION_ADMIN = ShipStationModelInitializer(
+    sdk=ShipStation(
+        "d73a353ab1a94f50a3b74acce14d499e", "e11ecf2cd9644cfd81d7d2cceaee65ae"
+    )
+)
